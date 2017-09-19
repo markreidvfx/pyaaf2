0cfabe9aa01e2caa0c48ed3192c1979b5d7827cd
Mark Reid <mindmark@gmail.com>
2017-09-18 20:22:06 -0700

    add some component classes

--- aaf2/components.py
+++ aaf2/components.py
@@ -9,3 +9,24 @@ from uuid import UUID
 
 from . import core
 from .utils import register_class
+
+class Component(core.AAFObject):
+    class_id = UUID("0d010101-0101-0200-060e-2b3402060101")
+
+class Segment(Component):
+    class_id = UUID("0d010101-0101-0300-060e-2b3402060101")
+
+@register_class
+class Sequence(Segment):
+    class_id = UUID("0d010101-0101-0f00-060e-2b3402060101")
+
+@register_class
+class NestedScope(Segment):
+    class_id = UUID("0d010101-0101-0b00-060e-2b3402060101")
+
+class SourceReference(Segment):
+    class_id = UUID("0d010101-0101-1000-060e-2b3402060101")
+
+@register_class
+class SourceClip(SourceReference):
+    class_id = UUID("0d010101-0101-1100-060e-2b3402060101")
