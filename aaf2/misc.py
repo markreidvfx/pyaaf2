b93af580670c45fb21132ac73a575268cad79856
Mark Reid <mindmark@gmail.com>
2018-07-02 21:30:48 -0700

    added rough CubicInterpolator, still needs testing

--- aaf2/misc.py
+++ aaf2/misc.py
@@ -184,6 +184,33 @@ def cubic_bezier(p0, p1, p2, p3, t):
             t * t * t * p3
     return value
 
+def cubic_catmullrom(p0, p1, p2, p3, t):
+
+    tt = t * t
+    ttt = tt * t
+
+    q0 = -ttt + (2.0 * tt) - t
+    q1 = (3.0 * ttt) - (5.0 * tt) + 2.0
+    q2 = (-3.0 * ttt) + (4.0 * tt) + t
+    q3 = ttt - tt
+
+    value = (p0 * q0) + (p1 * q1) + (p2 * q2) + (p3 * q3)
+    value *= 0.5
+    return value
+
+def cubic_interp(p0, p1, m0, m1, t,):
+    t = float(t)
+    tt = t * t
+    ttt = tt * t
+
+    q0 = (2.0 * ttt) - (3.0 * tt) + 1
+    q1 = ttt - (2.0 * tt) + t
+    q2 = (-2.0 * ttt) + (3.0 * tt)
+    q3 = ttt - tt
+
+    value = (q0 * p0) + (q1 * m0) + (q2 * p1) + (q3 * m1)
+    return value
+
 def cubic_bezier_interpolate(p0, p1, p2, p3, t):
 
     t_len = p3[0] - p0[0]
@@ -201,11 +228,119 @@ def cubic_bezier_interpolate(p0, p1, p2, p3, t):
             break
         offset = x - t
         guess_t -= offset/t_len
+
+        if guess_t < 0:
+            guess_t = 0
+        if guess_t > 1.0:
+            guess_t = 1.0
+
         # print("  ", offset)
 
     y = cubic_bezier(p0[1], p1[1], p2[1], p3[1], guess_t)
     # print(t, x, y)
 
+    return y
+
+def cubic_catmullrom_interpolate(p0, p1, p2, p3, t):
+    t_len = p2[0] - p1[0]
+    t_diff = t - p1[0]
+    t_mix = t_diff/t_len
+
+    guess_t = t_mix
+
+    for i in range(20):
+        x = cubic_catmullrom(p0[0], p1[0], p2[0], p3[0], guess_t)
+        if x == t:
+            break
+        offset = x - t
+        guess_t -= offset/t_len
+        # print("  ", offset)
+    # print(p0, p1, p2, p3, t_mix)
+
+    y = cubic_catmullrom(p0[1], p1[1], p2[1], p3[1], guess_t)
+    # print(t, x, y)
+    return y
+
+def cubic_interpolate(p0, p1, p2, p3, t):
+    # Note:
+    # This code is a lot of guess work
+    # not sure what Avid MC actually does
+    # MC docs refer to spline as "Natural Spline" or "Cardinal Spline"
+    # and AAF files export it with CubicInterpolator definition
+    # This is a hacked Cardinal spline that "looks" pretty close...
+
+    # Finite difference
+    # https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Finite_difference
+
+    # m0x = (p2[0] - p1[0]) + (p1[0] - p0[0])
+    # m0y = (p2[1] - p1[1]) + (p1[1] - p0[1])
+    #
+    # m1x = (p3[0] - p2[0]) + (p2[0] - p1[0])
+    # m1y = (p3[1] - p2[1]) + (p2[1] - p1[1])
+
+    # Cardinal spline
+    # https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Cardinal_spline
+
+    # m0x = (p2[0] - p0[0])
+    # m0y = (p2[1] - p0[1])
+    #
+    # m1x = (p3[0] - p2[0])
+    # m1y = (p3[1] - p2[1])
+
+    # Note: the tangets are both really the same??
+    m0x = (p3[0] - p0[0])
+    m0y = (p3[1] - p0[1])
+
+    m1x = (p3[0] - p0[0])
+    m1y = (p3[1] - p0[1])
+
+    # tension control
+    c = 0.75
+
+    flatten = 0
+    # MC appears to flatten tangets if point is
+    # higher/lower the its neighbor points
+    if p1[1] >= p0[1] and p1[1] >= p2[1]:
+        m0y *= flatten
+
+    if p1[1] <= p0[1] and p1[1] <= p2[1]:
+        m0y *= flatten
+
+    if p2[1] >= p3[1] and p2[1] >= p1[1]:
+        m1y *= flatten
+
+    if p2[1] <= p3[1] and p2[1] <= p1[1]:
+        m1y *= flatten
+
+    m0x *= c * 0.5
+    m0y *= c * 0.5
+
+    m1x *= c * 0.5
+    m1y *= c * 0.5
+
+    t_len = p2[0] - p1[0]
+    t_diff = t - p1[0]
+    t_mix = t_diff/t_len
+
+    guess_t = t_mix
+
+    for i in range(20):
+        x =  cubic_interp(p1[0], p2[0], m0x, m1x, guess_t)
+        if x == t:
+            break
+        offset = x - t
+        guess_t -= offset/t_len
+
+        if guess_t < 0:
+            guess_t = 0
+        if guess_t > 1.0:
+            guess_t = 1.0
+
+    c = c
+
+    y = cubic_interp(p1[1], p2[1], m0y, m1y, guess_t)
+
+    # print(t, x, y, i, t_mix, guess_t)
     return y
 
 
@@ -264,11 +399,39 @@ class VaryingValue(Parameter):
                                             (t3, v3), t)
 
         # CubicInterpolator
-        # elif self.interpolation.uuid  == UUID("a04a5439-8a0e-4cb7-975f-a5b255866883"):
-        #
-        #     v0 = float(bounds[0].value)
-        #     v1 = float(bounds[1].value)
-        #     return lerp(v0, v1, t_mix)
+        elif self.interpolation.uuid  == UUID("a04a5439-8a0e-4cb7-975f-a5b255866883"):
+
+            t1 = float(bounds[0].time)
+            v1 = float(bounds[0].value)
+
+            t2 = float(bounds[1].time)
+            v2 = float(bounds[1].value)
+
+            start = self.bounds(t1 - .0001)
+            end = self.bounds(t2 + .0001)
+
+            # print('s', t, bounds[0].time, [float(item.time) for item in start])
+            # print('e', t, bounds[1].time, [float(item.time) for item in end])
+
+            if len(start) == 1:
+                t0 = t1 - ((t2 - t1) * 0.5)
+                v0 = v1
+            else:
+                t0 = float(start[0].time)
+                v0 = float(start[0].value)
+
+            if len(end) == 1:
+                t3 = t2 + ((t2 - t1) * 0.5)
+                v3 = v2
+            else:
+                t3 = float(end[1].time)
+                v3 = float(end[1].value)
+
+            # return lerp(v1, v2, t_mix)
+            return cubic_interpolate((t0, v0),
+                                     (t1, v1),
+                                     (t2, v2),
+                                     (t3, v3), t)
 
 
         else:
