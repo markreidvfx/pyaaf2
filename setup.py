fa0ba6ddcb494f1cea068c3e5745cfada4392321
Mark Reid <mindmark@gmail.com>
2018-11-11 18:23:16 -0800

    version 1.0.0 release!

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import setuptools.command.build_py
 
 PROJECT_METADATA = {
-    "version": "1.0.0.dev5",
+    "version": "1.0.0",
     "author": 'Mark Reid',
     "author_email": 'mindmark@gmail.com',
     "license": 'MIT',
