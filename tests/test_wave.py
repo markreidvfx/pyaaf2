84ed43775214d6e93457251c2241cfe3276c9aa3
Jamie Hardt <jamiehardt@me.com>
2018-12-23 14:52:55 -0800

    Added wav link test

---
+++ tests/test_wave.py
@@ -0,0 +1,40 @@
+from __future__ import (
+    unicode_literals,
+    absolute_import,
+    print_function,
+    division,
+    )
+import os
+import subprocess
+import unittest
+import hashlib
+import common
+import aaf2
+from uuid import UUID
+from aaf2 import video, audio, mobid, mobs, essence
+
+
+class WAVETests(unittest.TestCase):
+    
+    def test_wav_link(self):
+        test_outfile = os.path.join(common.sandbox(), 'wav_link.aaf')
+
+        test_link_targets = [
+            generate_pcm_audio_mono('testmono', sample_rate=48000, duration=10,
+                sample_format='s16le', fmt='wav'),
+            generate_pcm_audio_mono('testmono24', sample_rate=48000, duration=10,
+                sample_format='s24le', fmt='wav'),
+            generate_pcm_audio_stereo('testmono', sample_rate=48000, duration=10,
+                sample_format='s24le', fmt='wav')
+                ]
+
+        with aaf2.open(test_outfile,'w') as f:
+            for target in test_link_targets:
+                meta = common.probe(target)
+                mobs = f.content.link_external_wav(meta)
+                
+
+if __name__ == "__main__":
+    import logging
+    # logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
