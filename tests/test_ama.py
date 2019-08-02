ba128506bbe1c560df7c173e97da4fb21f821c69
Jamie Hardt <jamiehardt@me.com>
2019-08-01 18:50:30 -0700

    Code style stuff

--- tests/test_ama.py
+++ tests/test_ama.py
@@ -3,29 +3,28 @@ from __future__ import (
     absolute_import,
     print_function,
     division,
-    )
+)
 import os
 import unittest
 import common
 import aaf2
 from aaf2 import ama
 
-
 avc_profiles = [('yuv420p', 'baseline'),
                 ('yuv420p', 'main'),
                 ('yuv420p', 'high'),
                 ('yuv422p', 'high422'),
-                #('yuv444p', 'high444')# unsupported in MC?
+                # ('yuv444p', 'high444')# unsupported in MC?
                 ]
 
-
 prores_profiles = [
-(0, 'proxy'),
-(1, 'LT'),
-(2, 'standard'),
-(3, 'HQ'),
-(4, '4444'),
+    (0, 'proxy'),
+    (1, 'LT'),
+    (2, 'standard'),
+    (3, 'HQ'),
+    (4, '4444'),
 ]
+
 
 class AMATests(unittest.TestCase):
 
@@ -82,9 +81,8 @@ class AMATests(unittest.TestCase):
             else:
                 self.fail("Tape SourceMob descriptor must be either a TapeDescriptor or ImportDescriptor")
 
-
         for chan_num in range(expected_sound_slots):
-            self.assertEqual(physical_sound_tracks.count(chan_num+1), 1,
+            self.assertEqual(physical_sound_tracks.count(chan_num + 1), 1,
                              "Incorrect PhysicalTrackNumber property on master mob slot")
 
         self.assertEqual(len(physical_sound_tracks), expected_sound_slots,
@@ -118,18 +116,18 @@ class AMATests(unittest.TestCase):
     def test_monoaural_wav(self):
         new_file = os.path.join(common.sandbox(), 'ama_wav.aaf')
         with aaf2.open(new_file, 'w') as f:
-
             wavfile = common.generate_pcm_audio_mono('test_ama.wav', fmt='wav')
             meta = common.probe(wavfile)
             mobs = ama.create_wav_link(f, meta)
-            self.assertTrue(len(mobs),3)
+            self.assertTrue(len(mobs), 3)
 
         with aaf2.open(new_file, 'r') as f:
             common.walk_aaf(f.root)
             self.assertTrue(len(f.content.mobs) == 3)
-            self.assertTrue(len(list(f.content.mastermobs()) ) == 1 )
+            self.assertTrue(len(list(f.content.mastermobs())) == 1)
             master_mob = next(f.content.mastermobs())
-            self.assert_mastermob_valid_edit_spec(master_mob=master_mob, expected_sound_slots=1, expected_picture_slots=0)
+            self.assert_mastermob_valid_edit_spec(master_mob=master_mob, expected_sound_slots=1,
+                                                  expected_picture_slots=0)
             self.assertEqual(len(master_mob.slots), 1, "MasterMob should only have one slot")
             self.assertEqual(master_mob.slots[0].media_kind, 'Sound', "MasterMob slot has incorrect media_kind")
             source_clip = master_mob.slots[0].segment.components[0]
@@ -144,19 +142,19 @@ class AMATests(unittest.TestCase):
             audio_channel_count = 2
             created_mastermob_ids = []
             for (pix_fmt, profile) in avc_profiles:
-                vcodec = ['-pix_fmt', pix_fmt, '-c:v', 'h264', '-profile:v', profile ]
+                vcodec = ['-pix_fmt', pix_fmt, '-c:v', 'h264', '-profile:v', profile]
 
-                mov = common.generate_mov('ama_avc_%s.mov' % profile, overwrite=False, vcodec=vcodec, audio_channels=audio_channel_count)
+                mov = common.generate_mov('ama_avc_%s.mov' % profile, overwrite=False, vcodec=vcodec,
+                                          audio_channels=audio_channel_count)
                 meta = common.probe(mov)
                 # print(meta['streams'][0]['profile'])
 
                 mobs = f.content.create_ama_link(mov, meta)
-                self.assertEqual(len(mobs),3)
+                self.assertEqual(len(mobs), 3)
                 self.assertIsInstance(mobs[0], aaf2.mobs.MasterMob)
                 self.assertIsInstance(mobs[1], aaf2.mobs.SourceMob)
                 self.assertIsInstance(mobs[2], aaf2.mobs.SourceMob)
                 created_mastermob_ids.append(mobs[0].mob_id)
-
 
         with aaf2.open(new_file, 'r') as f:
             common.walk_aaf(f.root)
@@ -177,7 +175,8 @@ class AMATests(unittest.TestCase):
 
             for profile, name in prores_profiles:
                 vcodec = ['-c:v', 'prores_ks', '-profile:v', str(profile)]
-                mov = common.generate_mov('ama_prores_%s.mov' % (name,), overwrite=False, vcodec=vcodec, audio_channels=2)
+                mov = common.generate_mov('ama_prores_%s.mov' % (name,), overwrite=False, vcodec=vcodec,
+                                          audio_channels=2)
                 meta = common.probe(mov)
                 mobs = f.content.create_ama_link(mov, meta)
                 self.assertEqual(len(mobs), 3, "create_ama_link must return exactly three mobs")
@@ -195,7 +194,9 @@ class AMATests(unittest.TestCase):
                 self.assert_mastermob_valid_edit_spec(mastermob, expected_picture_slots=1, expected_sound_slots=2)
                 self.assert_valid_multiple_descriptor(mastermob, 2)
 
+
 if __name__ == "__main__":
     import logging
+
     # logging.basicConfig(level=logging.DEBUG)
     unittest.main()
