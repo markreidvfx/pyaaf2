9722f4cac4a7832dd163c19508ca266368b81370
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-07-09 17:44:06 -0400

    Always close AAFFile's resources when exiting context manager

--- tests/test_aaf.py
+++ tests/test_aaf.py
@@ -36,6 +36,143 @@ class AAFTests(unittest.TestCase):
         with aaf2.open(new_file, 'r') as f:
             common.walk_aaf(f.root)
 
+    def test_exit_with_exception_no_save(self):
+        new_file = os.path.join(common.sandbox(), 'save_with_exception_no_save.aaf')
+        test_file = common.test_file_01()
+        shutil.copy(test_file, new_file)
+
+        with self.assertRaises(ValueError):
+            with aaf2.open(new_file, 'r+') as f:
+                # mobs = f.content.mobs
+                mob = f.create.MasterMob("TestExitMob")
+                f.content.mobs.append(mob)
+
+                raise ValueError('asd')
+
+        self.assertTrue(f.f.closed)
+        self.assertFalse(f.is_open)
+        self.assertFalse(f.cfb.is_open)
+
+        with open(test_file, 'rb') as testFile:
+            with open(new_file, 'rb') as newFile:
+                self.assertNotEqual(testFile.read(), newFile.read())
+
+        with aaf2.open(new_file, 'r') as f:
+            # Make sure that the file can still be opened with aaf2.open
+            # and that the save method wasn't run.
+            self.assertIsNone(f.content.mobs.get('TestExitMob'))
+
+    def test_exit_with_exception_with_save(self):
+
+        new_file = os.path.join(common.sandbox(), 'save_with_exception_with_save.aaf')
+        test_file = common.test_file_01()
+        shutil.copy(test_file, new_file)
+
+        with self.assertRaises(ValueError):
+            with aaf2.open(new_file, 'r+') as f:
+                # mobs = f.content.mobs
+                mob = f.create.MasterMob("TestExitMob")
+                f.content.mobs.append(mob)
+
+                f.save()
+
+                raise ValueError('asd')
+
+        self.assertTrue(f.f.closed)
+        self.assertFalse(f.is_open)
+        self.assertFalse(f.cfb.is_open)
+
+        with open(test_file, 'rb') as testFile:
+            with open(new_file, 'rb') as newFile:
+                self.assertNotEqual(testFile.read(), newFile.read())
+
+        with aaf2.open(new_file, 'r') as f:
+            # Make sure that the file can still be opened with aaf2.open
+            # and that the save method wasn't run.
+            self.assertIsNotNone(f.content.mobs.get('TestExitMob'))
+
+    def test_exit_with_internal_exception(self):
+        """This scenario is a bad situation... explosion
+        Exercise the scenario where no exception occurs inside with with block, but
+        an exception occurs during the exit phase of the context manager.
+        """
+
+        new_file = os.path.join(common.sandbox(), 'save_with_exception_with_save.aaf')
+        test_file = common.test_file_01()
+        shutil.copy(test_file, new_file)
+
+        with self.assertRaises(RuntimeError):
+            with aaf2.open(new_file, 'r+') as f:
+                def mock(*args, **kwargs):
+                    raise RuntimeError('asd')
+
+                # This will result in a corrupted file
+                f.cfb.close = mock
+
+                # mobs = f.content.mobs
+                mob = f.create.MasterMob("TestExitMob")
+                f.content.mobs.append(mob)
+
+                f.save()
+
+        self.assertFalse(f.f.closed)
+        self.assertTrue(f.is_open)
+        self.assertTrue(f.cfb.is_open)
+
+        with open(test_file, 'rb') as testFile:
+            with open(new_file, 'rb') as newFile:
+                self.assertNotEqual(testFile.read(), newFile.read())
+
+        with self.assertRaises(IndexError):
+            # new_file is now corrupted.
+            with aaf2.open(new_file, 'r') as f:
+                pass
+
+    def test_exit_with_internal_and_external_exception(self):
+        # Exception occurs in with block and also in the except clause
+        # while closing file descriptors.
+
+        new_file = os.path.join(common.sandbox(), 'save_with_exception_with_save.aaf')
+        test_file = common.test_file_01()
+        shutil.copy(test_file, new_file)
+
+        # Notice how the exception propagated is not a ValueError.
+        with self.assertRaises(RuntimeError):
+            with aaf2.open(new_file, 'r+') as f:
+                def mock(*args, **kwargs):
+                    raise RuntimeError('asd')
+
+                # This will result in a corrupted file
+                f.cfb.close = mock
+
+                # mobs = f.content.mobs
+                mob = f.create.MasterMob("TestExitMob")
+                f.content.mobs.append(mob)
+
+                f.save()
+
+                raise ValueError('asd')
+
+        # And also the file is still closed even if the cfb failed to be closed.
+        self.assertTrue(f.f.closed)
+        self.assertFalse(f.is_open)
+        self.assertTrue(f.cfb.is_open)
+
+        with open(test_file, 'rb') as testFile:
+            with open(new_file, 'rb') as newFile:
+                self.assertNotEqual(testFile.read(), newFile.read())
+
+        with self.assertRaises(IndexError):
+            # new_file is now corrupted.
+            with aaf2.open(new_file, 'r') as f:
+                pass
+
+    def test_save_after_close(self):
+        aaf_file = aaf2.open()
+        aaf_file.close()
+        with self.assertRaises(IOError):
+            aaf_file.save()
+
 
 if __name__ == "__main__":
     import logging
